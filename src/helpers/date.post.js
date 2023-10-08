6c229420984d5ede963c8d538df1f81b14d782ae
Main <gimenezfrancotomas96@gmail.com>
2023-10-08 16:35:02 -0300

    modificacion funcion getDates()

--- src/helpers/date.post.js
+++ src/helpers/date.post.js
@@ -1,21 +1,13 @@
-const today = new Date()
-
-function getDates(startDate, endDate, limitDays = 1) {
+function getDates( limitDays = 1, startDate) {
     if (!startDate) {
         startDate = new Date()
     }
-    
-    if (!endDate) {
-        const daysFromNow = startDate
-        daysFromNow.setDate(daysFromNow.getDate() + limitDays)
-        endDate = daysFromNow
-    }
 
+    const oneDay = 24 * 60 * 60 * 1000
     const dates = []
     let currentDate = startDate
-    const oneDay = 24 * 60 * 60 * 1000
 
-    while (currentDate <= endDate) {
+    while (dates.length < limitDays) {
         dates.push(new Date(currentDate))
         currentDate = new Date(currentDate.getTime() + oneDay)
     }
