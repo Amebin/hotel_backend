57d0d793b0206cb0c559f5c3dd23152053096eac
Main <gimenezfrancotomas96@gmail.com>
2023-10-19 23:38:16 -0300

    creacion de reserved routes

--- src/routes/reserved.routes.js
+++ src/routes/reserved.routes.js
@@ -3,25 +3,25 @@ import { Router } from 'express'
 
 import roomModel from '../models/rooms.models.js'
 import reservationModel from '../models/reserved.models.js'
-import { avoidConsecutiveSpaces, isIsoDate, verifyToken, checkRoles, checkRequired } from '../middlewares/rooms.middleware.js'
+import { verifyToken, checkRoles, checkRequired } from '../middlewares/rooms.middleware.js'
 
 import { body, validationResult } from 'express-validator'
 
 export const reservedRoutes = ()  => {
     const router = Router()
 
-    const validateCreateFields = [
-        userID = req.body.userID,
-        roomID = req.body.roomID,
-        date = req.body.date
-    ]
+    router.get('/reservation', verifyToken, async (req, res) => {
+        const id = req.userId
+        const reserved = await reservationModel.find({ user: id })
+        res.status(200).send({ status: 'OK', data: reserved })       
+    })
 
-    router.get('/', async (req, res) => {
+    router.get('/admin/reservations', verifyToken, checkRoles(['admin']), async (req, res) => {
         const reserved = await reservationModel.find()
         res.status(200).send({ status: 'OK', data: reserved })
     })
 
-    router.get('/one/:rid', async (req, res) => {
+    router.get('/admin/onereservation/:rid', verifyToken, checkRoles(['admin']), async (req, res) => {
         try {
             if (mongoose.Types.ObjectId.isValid(req.params.rid)) {
                 const reserved = await reservationModel.findById(req.params.rid)
@@ -40,4 +40,25 @@ export const reservedRoutes = ()  => {
     }
     )
 
+    router.delete('/admin/deletereservation/:rid', verifyToken, checkRoles(['admin']), async (req, res) => {
+        try {
+            const id = req.params.rid
+            if (mongoose.Types.ObjectId.isValid(id)) {
+                const reservedToDelete = await reservationModel.findOneAndDelete({ _id: id })
+
+                if (!reservedToDelete) {
+                    res.status(404).send({ status: 'ERR', data: 'No existe reserva con ese ID' })
+                } else {
+                    res.status(200).send({ status: 'OK', data: reservedToDelete })
+                }
+            } else {
+                res.status(400).send({ status: 'ERR', data: 'Formato de ID no válido' })
+            }
+        } catch (err) {
+            res.status(500).send({ status: 'ERR', data: err.message })
+        }
+    })
+
+
+    return router
 }
